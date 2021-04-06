20ada0908a364fc54e0fdb9a026e5b8dd2b9e0e1
charlierdm <charlie.davismiller@gmail.com>
2021-04-06 11:47:45 +0100

    fifth test passing, accounted for variable casing

--- lib/school_results.rb
+++ lib/school_results.rb
@@ -1,3 +1,22 @@
 class Grades
 
+  def scores(list)
+    return "No results given" if list.empty?
+
+    results = ["Green", "Amber", "Red"]
+
+    test_result = list.delete(" ").split(",").map { |result| result.capitalize }
+
+    output_array = []
+
+    results.map do |colour|
+      if test_result.count(colour) > 0
+        output_array.push("#{colour}: #{test_result.count(colour)}")
+      end
+    end
+
+    output_array.join("\n")
+
+  end
+
 end
--- spec/school_results_spec.rb
+++ spec/school_results_spec.rb
@@ -1,5 +1,52 @@
 require 'school_results'
 
 describe Grades do
+
+  it 'return No Results Given when passed an empty string' do
+    expect(subject.scores("")).to eq("No results given")
+  end
+
+  it 'returns a single colour and the count of it' do
+    expect(subject.scores("Green")).to eq("Green: 1")
+  end
+
+  it 'returns 2 colours and their respective counts' do
+    expect(subject.scores("Green, Red")).to eq("Green: 1\nRed: 1")
+  end
+
+  it 'returns 3 colours and their respective values' do
+    expect(subject.scores("Green, Amber, Red")).to eq("Green: 1\nAmber: 1\nRed: 1")
+  end
+
+  it 'responds to incorrect casing conventions' do
+    expect(subject.scores("red, Green, Amber, amber")).to eq("Green: 1\nAmber: 2\nRed: 1")
+  end
+
+
   
 end
+
+
+=begin
+  
+To create a reporting system based on a list of student grades.
+
+example: "green,Green, Red, Amber, red" -> "Green: 2\nAmber: 1\nRed: 2"
+
+input                               | output 
+
+""                                  "No results given."
+
+"Green"                             "Green: 1"
+
+"Green, Red"                        "Green: 1\nRed: 1"
+
+"Green, Amber, Red"                 "Green: 1\nAmber: 1\nRed: 1"
+
+"red, Green, Amber, amber"         "Green: 1\nAmber: 2\nRed: 1"
+
+"Red, blue, Green, green, yellow"   "Green: 2\nRed: 1\nUncounted: 2"
+
+"amber,green, Green, orange, Red"    "Green: 2\nAmber: 1\nRed: 1\nUncounted: 1"
+
+=end
